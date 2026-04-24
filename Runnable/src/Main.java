3ff2cbf12d59c1826b2fbef3212c7ed706a63f3f
khanhld3010 <luuduckhanh3010@gmail.com>
2026-04-24 14:38:13 +0700

    RunnableDemo

---
+++ Runnable/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static void main(String[] args) {
+        System.out.println("Main thread running... ");
+        RunnableDemo runnableDemo1 = new RunnableDemo("Thread-1-HR-Database");
+        runnableDemo1.start();
+        RunnableDemo runnableDemo2 = new RunnableDemo("Thread-2-HR-Database");
+        runnableDemo2.start();
+        System.out.println("Main thread stopped!!! ");
+    }
+}
