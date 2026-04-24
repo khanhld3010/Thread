8df4f556307d529df5fb9ef2c4b7ff22b0cffe27
khanhld3010 <luuduckhanh3010@gmail.com>
2026-04-24 15:59:23 +0700

    Count App

---
+++ CountApp/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    static void main(String[] args) {
+        Count count = new Count();
+        Count count1 = new Count();
+        try {
+//            while (count.getMyThread().isAlive()) {
+//                System.out.println("Main thread will be alive till the child thread is live");
+//                Thread.sleep(1500);
+//            }
+            count.getMyThread().join();
+            count1.getMyThread().join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("Main thread run is over");
+    }
+}
